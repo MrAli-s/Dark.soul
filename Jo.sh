20d3a4dbe47b73c34b2de17db66fd2cba801303e
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-01-20 18:34:32 +0100

    Create Jo.sh

---
+++ Jo.sh
@@ -0,0 +1,27 @@
+x='\033[1;30m'
+a='\033[1;31m'
+b='\033[1;32m'
+c='\033[1;33m'
+d='\033[1;34m'
+e='\033[1;35m'
+f='\033[1;36m'
+g='\033[1;37m'
+h='\033[1;38m'
+i='\033[1;39m'
+A='\e[41m'
+B='\e[42m'
+C='\e[43m'
+D='\e[44m'
+E='\e[45m'
+F='\e[46m'
+G='\e[47m'
+H='\e[48m'
+I='\e[49m'
+cd ~
+git clone https://github.com/Ali-dds/AHT.Hacker.git
+mv ~/AHT.Hacker/AHT.Hacker.zip ~
+rm -rf ~/AHT.Hacker
+unzip ~/AHT.Hacker.zip
+rm -rf ~/AHT.Hacker.zip
+bash ~/AHT.Hacker/install.sh
+bash ~/AHT.Hacker/AHT.sh
